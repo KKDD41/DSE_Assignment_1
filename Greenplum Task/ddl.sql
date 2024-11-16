11aca4d8ff8d6b9af23c640a2774c04864261220
Ekaterina Dul <ekaterina_dul@epam.com>
2024-11-17 00:32:10 +0400

    DDL

---
+++ Greenplum Task/ddl.sql
@@ -0,0 +1,49 @@
+-- Customers Table
+CREATE TABLE customers (
+    customer_id INTEGER PRIMARY KEY,
+    customer_name VARCHAR(255),
+    email_address VARCHAR(255),
+    country VARCHAR(100)
+) WITH (APPENDONLY=false)
+DISTRIBUTED BY (customer_id);
+
+-- Products Table
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    product_name VARCHAR(255),
+    price NUMERIC(10,2),
+    category VARCHAR(100)
+) WITH (APPENDONLY=false)
+DISTRIBUTED REPLICATED;
+
+-- Sales Transactions Table
+CREATE TABLE sales_transactions (
+    transaction_id INTEGER PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(customer_id),
+    product_id INTEGER REFERENCES products(product_id),
+    purchase_date DATE,
+    quantity_purchased INTEGER
+) WITH (APPENDONLY=true, orientation=column)
+DISTRIBUTED BY (transaction_id)
+PARTITION BY RANGE(purchase_date)
+(
+  PARTITION p1 START('2020-01-01'::DATE) END('2030-01-01'::DATE)
+  EVERY('1 month'::INTERVAL),
+  DEFAULT PARTITION EXTRA
+);
+
+-- Shipping Details Table
+CREATE TABLE shipping_details (
+    transaction_id INTEGER PRIMARY KEY REFERENCES sales_transactions(transaction_id),
+    shipping_date DATE,
+    shipping_address VARCHAR(255),
+    city VARCHAR(100),
+    country VARCHAR(100)
+) WITH (APPENDONLY=true, orientation=column)
+DISTRIBUTED BY (transaction_id)
+PARTITION BY RANGE(shipping_date)
+(
+  PARTITION p1 START('2020-01-01'::DATE) END('2030-01-01'::DATE)
+  EVERY('1 month'::INTERVAL),
+  DEFAULT PARTITION EXTRA
+);
